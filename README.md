979e50a0da2bd49ea57376b3a74cff31f400da7c
Th3Trig3rFac3 <j.domonkos01@gmail.com>
2021-05-06 12:36:54 +0200

    Date: 2021.05.02 19:03

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# Webprog2_Beadando" 
+webprog beadandó: front-end hiányzik, user_register.php szabályok, register.php errorok kiíársa,
+homeot megírni (ez lesz az opening page?), képek megjelenítése szépen, az index.php nem enged el a login.php-ra, login.php is vihetne
+a register.php-ra, adatbázis táblákat megcsinálni
+regisztrációhoz: username, email-cím, jelszó 2x (hide/visible gomb?)
+mysql pagination
---
+++ private/components/nav.php
@@ -0,0 +1,43 @@
+<a class="navbar-brand" href="#">Element</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse col-auto" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="?p=home">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="?p=auth/login">Jelentkezz be</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="?p=auth/register">Regisztrálj</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="?p=about">About</a>
+        </li>
+        <!--
+        <?php //if(web2.beadando.admin = 0) ?>
+        <li class="nav-item">
+            <a class="nav-link" href="?p=admin">Admin panel</a>
+        </li>
+        csak akkor legyen látható a gomb, ha admin van bejelentkezve -->
+        <!--
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li> -->
+    </ul>
+    <!-- search mehetne ide -->
+</div>
