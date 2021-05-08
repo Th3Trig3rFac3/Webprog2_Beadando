f6a0dc20649eab5480bec0bf8f28b826e85a44ac
Th3Trig3rFac3 <j.domonkos01@gmail.com>
2021-05-08 15:39:09 +0200

    A footer a képernyő alján marad

--- index.php
+++ index.php
@@ -30,9 +30,9 @@ $PAGE = $_GET['p'] ?? 'home';
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <title>Element</title>
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
     <nav class="navbar navbar-expand-lg navbar-light bg-light"><?php require_once './private/components/nav.php' ?> </nav>
     <main><?php require_once 'private/lib/router.php'?></main>
-    <footer><?php require_once 'private/components/footer.php'?></footer>
+    <footer class="mt-auto"><?php require_once 'private/components/footer.php'?></footer>
 </body>
 </html>
