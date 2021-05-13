aa2652b873b93ac3d4d2d95a022fa9bfa1f2d9dc
Th3Trig3rFac3 <j.domonkos01@gmail.com>
2021-05-13 23:29:45 +0200

    receptek.php under fixing

--- private/pages/receptek.php
+++ private/pages/receptek.php
@@ -1,5 +1,6 @@
 <?php
-    $receptek = db_fetchAll('SELECT * FROM posts');
+    //$receptek = db_fetchAll('SELECT * FROM posts');
+    $receptek = db_fetchAll('SELECT posts.id, posts.name, posts.description, posts.Post_Time, users.id, users.username FROM posts, users WHERE users.id = posts.owner_id');
     $images = db_fetchAll('SELECT owner_Id, File_Name, posts.Post_Time as "Post_Time" FROM users, posts WHERE users.Id = posts.owner_Id'); //lehet szar, ne használd
 ?>
 
@@ -17,8 +18,8 @@
             <?= $recept['description'] ?>
         </p>
         <div class="card-footer m-1 row">
-            <span class="col">Feltöltés dátuma:<?= $recept['Post_Time'] ?> </span>
-            <span class="col">Feltöltő:<?= $recept['owner_Id'] ?> </span>       <!-- később javítani -->
+            <span class="col">Feltöltés dátuma: <?= $recept['Post_Time'] ?> </span>
+            <span class="col">Feltöltő: <?= $recept['users.username'] ?> </span>
         </div>
     </div>
 <?php endforeach; ?>
