35074cd7b482f55c0fc1550841fc325906e0822c
Th3Trig3rFac3 <j.domonkos01@gmail.com>
2021-05-13 10:03:50 +0200

    home.php kész

--- private/pages/home.php
+++ private/pages/home.php
@@ -1,2 +1,12 @@
 <!-- Ide jön a home menü-->
-home
+<div class="container col-auto">
+    <div class="text-center">
+        <h1 class="mt-5">Üdvözlöm az Element receptek weboldalán</h1>
+        <p class="lead">A weboldal célja, hogy a lelkes konyhai varázslók (legyen kezdő vagy tapasztalt) megtalálják vagy megosztsák kedvenc ételüket a közösséggel</p>
+        <ul class="list-unstyled">  <!--
+            <li>Bootstrap 4.6.0</li>
+            <li>jQuery 3.5.1</li>   -->
+        </ul>
+    </div>
+</div>
+
