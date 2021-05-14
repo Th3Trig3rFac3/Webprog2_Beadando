1ee90cb23d06ecae458d7014a8a484fbd80a3c97
Th3Trig3rFac3 <j.domonkos01@gmail.com>
2021-05-14 10:12:15 +0200

    bread: edit.php kész

--- private/pages/subreddit/edit.php
+++ private/pages/subreddit/edit.php
@@ -1,21 +1,40 @@
 <?php
-$subreddit = db_fetch('posts', 'Id = :id', [':id' => $_GET['r']]);
+$receptek = db_fetch('posts', 'Id = :id', [':id' => $_GET['r']]);
 $errors = [];
 
 if(isset($_POST['submit'])){
     require_once 'private/lib/utils/file.php'; //szar
-    require_once 'private/lib/validations/subreddit-icon.php'; //szar
 
-    if(empty($errors)){     //átírni sajátra
+    if(empty($errors)){
         $name = $_POST['name'];
         $description = $_POST['description'];
+        $id = $receptek['Id'];
 
-        db_execute('UPDATE posts SET name = :name, description = :descriptionWHERE id = :id', [
+        db_execute('UPDATE posts SET name = :name, description = :description WHERE id = :id', [
             ':name' => $name,
             ':description' => $description,
-            ':id' => $subreddit['id'],
+            ':id' => $receptek['Id'],
         ]);
     }
-}
+}?>
 
-?>
+<div>
+    <form method="post" enctype="multipart/form-data" class="col-auto">
+        <div class="col-auto h5 mb-5">
+            <laber for='name'>Recept új neve</laber>
+            <input type="text" name="name" id="name" value="<?= $receptek['name'] ?>" required>
+            <?php if (isset($errors['name'])): ?>
+                <span class="alert alert-danger"><?= implode(', ', ($errors['name'] ?? [])) ?> </span>
+            <?php endif; ?>
+        </div>
+        <div class="col-auto h5 mb-5">
+            <label for="description">Új leírás</label>
+            <!-- <input type="text" id="description" name="description" required> -->
+            <textarea name="description" id="description" required><?= $receptek['description'] ?></textarea>
+            <?php if (isset($errors['description'])): ?>
+                <span class="alert alert-danger"><?= implode(', ', ($errors['description'] ?? [])) ?> </span>
+            <?php endif; ?>
+        </div>
+        <button type="submit" class="btn btn-primary col-auto h3 mb-3" value="UPDATE" name="submit">Frissítés</button>
+    </form>
+</div>
